e3d04486f87a256fbffd60c6afa471d58e7be9c9
gladysherrera <profherreratic@gmail.com>
2024-11-25 20:37:19 -0300

    creacion de juego (clase padre) y modificacion de paseIngles

---
+++ clases/juego.ts
@@ -0,0 +1,43 @@
+import { JuegoCasino } from "../interfaz/juegoCasino";
+import { Cliente } from "./Cliente";
+
+export abstract class Juego implements JuegoCasino{
+    protected nombre : string = "";
+    protected apuestaMinima: number = 0;
+    protected apuestaMaxima: number = 100000;
+
+    constructor () {}
+
+    public abstract apostar(jugador: Cliente): void 
+
+    public abstract pagar():number;
+
+    public abstract jugar():void;
+
+    public getNombre():string {
+        return this.nombre;
+    }
+    public setNombre(value: string) {
+        if (value != undefined && value.trim() != "") {
+            this.nombre = value.trim();
+        }
+    }
+    public getApuestaMinima(): number {
+        return this.apuestaMinima;
+    }
+    public  setApuestaMinima(value: number) {
+        if (value != undefined && value > 0) {
+            this.apuestaMinima = value;
+        }
+    }
+
+    public getApuestaMaxima(): number {
+        return this.apuestaMinima;
+    }
+    public  setApuestaMaxima(value: number) {
+        if (value != undefined && value > 0) {
+            this.apuestaMaxima = value;
+        }
+
+    }
+}
