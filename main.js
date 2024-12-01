918642c0d5a62e642cb3b6577b187cc527bf24cd
MiguelE1973 <miguel73etcheverry@gmail.com>
2024-12-01 15:50:46 -0300

    sin js

--- main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Casino_1 = require("./clases/Casino");
-var casino = new Casino_1.Casino();
-console.clear();
-casino.abrirCasino();
